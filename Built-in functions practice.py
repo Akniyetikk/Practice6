930946cdfcf642d9056e443092d5b16a93a6ab1b
Akniyet <aknietnroe@gmail.com>
2026-03-07 14:43:25 +0500

    Update Built-in functions practice.py

--- Built-in functions practice.py
+++ Built-in functions practice.py
@@ -1,21 +1,21 @@
 from functools import reduce
 
 data = ["10", "25", "30", "45", "50"]
-names = ["Товар А", "Товар Б", "Товар В"]
+names = ["product A", "product B", "product C"]
 prices = [1000, 2500, 3000]
 
 numbers = list(map(int, data))
-print(f"Числа: {numbers}")
+print(numbers)
 
 filtered = list(filter(lambda x: x > 25, numbers))
-print(f"Числа > 25: {filtered}")
+print(filtered)
 
 total_sum = reduce(lambda x, y: x + y, numbers)
-print(f"Сумма через reduce: {total_sum}")
+print(total_sum)
 
-print("\nОтчет по товарам:")
+print("\nОтчет:")
 for i, (name, price) in enumerate(zip(names, prices), 1):
     type_status = "OK" if isinstance(price, int) else "Error"
-    print(f"{i}. {name} — {price} руб. [Статус типа: {type_status}]")
+    print(f"{i}. {name} — {price} tg. [Type status: {type_status}]")
 
-print(f"\nСортировка цен: {sorted(prices, reverse=True)}")
+print(f"\n {sorted(prices, reverse=True)}")
