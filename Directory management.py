ddccc9fbcf6307258f8962575b5a054b73b6767b
Akniyet <aknietnroe@gmail.com>
2026-03-07 14:12:26 +0500

    Update Directory management.py

--- Directory management.py
+++ Directory management.py
@@ -1,18 +1,17 @@
 import os
 
-# Узнаем, где мы
 print("Я тут:", os.getcwd())
 
-# Создаем сложную структуру папок
+
 os.makedirs("projects/python/scripts", exist_ok=True)
 
-# Переходим внутрь
+
 os.chdir("projects/python")
 print("Теперь я тут:", os.getcwd())
 
-# Смотрим, что внутри (сейчас там только папка 'scripts')
+
 items = os.listdir(".")
 print("Содержимое:", items)
 
-# Попробуем удалить папку (она пустая, так что сработает)
+
 os.rmdir("scripts")
