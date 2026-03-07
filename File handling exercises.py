cd8c7767f15e5af43816e7f023d874b964cb2d69
Akniyet <aknietnroe@gmail.com>
2026-03-07 14:49:45 +0500

    Update File handling exercises.py

--- File handling exercises.py
+++ File handling exercises.py
@@ -2,20 +2,20 @@ import shutil
 from pathlib import Path
 
 file = Path("test_data.txt")
-file.write_text("Линия 1: Начальные данные\n", encoding="utf-8")
-print("--- Файл создан.")
+file.write_text("Line 1: Initial data\n", encoding="utf-8")
+print("-File is created.")
 
 with file.open("a", encoding="utf-8") as f:
-    f.write("Линия 2: Добавленные данные\n")
-print("--- Данные добавлены.")
+    f.write("Line 2: Added data\n")
+print("-Data is added.")
 
 content = file.read_text(encoding="utf-8")
-print(f"--- Содержимое файла:\n{content}")
+print(f"-Content:\n{content}")
 
 backup_file = Path("test_data_backup.txt")
 shutil.copy(file, backup_file)
-print(f"--- Создана копия: {backup_file.name}")
+print(f"-Backup file: {backup_file.name}")
 
 if backup_file.exists():
     backup_file.unlink()
-    print("--- Копия удалена для чистоты.")
+    print("-Backup was deleted")
