41a2d1e77d21a5457672e64c7e1a7bc1be0f4966
Akniyet <aknietnroe@gmail.com>
2026-03-07 14:58:40 +0500

    Update Built-in functions.py

--- Built-in functions.py
+++ Built-in functions.py
@@ -1,35 +1,32 @@
 from functools import reduce
 
 numbers = [10, 2, 8, 4, 6]
-names = ["Анна", "Борис", "Виктор"]
+names = ["Akniyet", "Aigul", "Azamat"]
 scores = [85, 92, 78]
 
-print(f"Длина списка: {len(numbers)}")        # 5
-print(f"Сумма всех чисел: {sum(numbers)}")     # 30
-print(f"Минимум: {min(numbers)}")              # 2
-print(f"Максимум: {max(numbers)}")             # 10
-print(f"Новый отсортированный список: {sorted(numbers)}") # [2, 4, 6, 8, 10]
+print(f"Length: {len(numbers)}")  
+print(f"Sum: {sum(numbers)}")    
+print(f"Min: {min(numbers)}")    
+print(f"Max: {max(numbers)}")     
+print(f"Sorted list: {sorted(numbers)}") 
 
-print(f"Строка в число: {int('123') + 7}")     # 130
-print(f"Число в строку: {str(10) + ' руб.'}")  # "10 руб."
-print(f"В список: {list('ABC')}")              # ['A', 'B', 'C']
-print(f"Логическое значение: {bool(1)}")       # True (0 - это False)
+print(f"Str to int: {int('123') + 7}")    
+print(f"Int to str: {str(10) + ' tg.'}")  
+print(f"List: {list('ABC')}")    
+print(f"Logical meaning: {bool(1)}")      
 
 squares = list(map(lambda x: x**2, numbers)) 
-print(f"Квадраты: {squares}") # [100, 4, 64, 16, 36]
+print(f"Squares: {squares}") 
 
 big_nums = list(filter(lambda x: x > 5, numbers))
-print(f"Числа > 5: {big_nums}") # [10, 8, 6]
+print(f"Numbers > 5: {big_nums}") 
 
 product = reduce(lambda x, y: x * y, numbers)
-print(f"Произведение всех чисел: {product}") # 3840
+print(f"Product: {product}") 
 
-print("\nСписок студентов:")
+print("\nlist of students")
 
 for i, (name, score) in enumerate(zip(names, scores), 1):
-    print(f"{i}. {name} получил {score}")
+    print(f"{i}. {name} got {score}")
 
-# Результат цикла:
-# 1. Анна получил 85
-# 2. Борис получил 92
-# 3. Виктор получил 78
+
